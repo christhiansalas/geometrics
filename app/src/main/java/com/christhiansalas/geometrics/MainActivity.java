fe434f72c45a3fecb5d13d44642701b8bebe13e9
christhiansalas@outlook.com <software123D>
2017-07-28 01:11:03 -0500

    Estructura de los activity Main y colores base.

--- app/src/main/java/com/christhiansalas/geometrics/MainActivity.java
+++ app/src/main/java/com/christhiansalas/geometrics/MainActivity.java
@@ -1,7 +1,9 @@
 package com.christhiansalas.geometrics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cuadrado(View view){
+
+        Intent intent =new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void triangulo(View view){
+
+        Intent intent =new Intent(MainActivity.this, Main3Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void circulo(View view){
+
+        Intent intent =new Intent(MainActivity.this, Main4Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void poligono(View view){
+
+        Intent intent =new Intent(MainActivity.this, Main5Activity.class);
+        startActivity(intent);
+
+    }
+
+
+
+
 }
