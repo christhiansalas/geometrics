e3abb4634251b7e8bda21a40a3d610a88830725e
Christhian Salas <christhiansalas@outlook.com>
2017-07-31 23:53:28 -0500

    Estructura de los activity Main y colores base.

--- app/src/main/java/com/christhiansalas/geometrics/Main2Activity.java
+++ app/src/main/java/com/christhiansalas/geometrics/Main2Activity.java
@@ -9,25 +9,13 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
-    Button btn_proceso;
-    TextView tv_perimetro, tv_area;
-    EditText et_ladoC;
-    String numero1 = "0"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        tv_perimetro = (TextView) findViewById(R.id.tv_perimetro);
-        tv_area = (TextView) findViewById(R.id.tv_area);
-        et_ladoC = (EditText) findViewById(R.id.et_ladoC);
 
-        btn_proceso.setOnClickListener(
-                et_ladoC.setText(numero1);
-
-
-        );
 
     }
 }
